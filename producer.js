d91b5e1e6d614d8464f7ca93768aa761f8502031
Julien Flusin <julienflusin3gmail.com>
2024-01-19 01:20:40 +0100

    update 4

--- producer.js
+++ producer.js
@@ -1,10 +1,14 @@
 import { process_tbm_data, send_messages } from './utility.js'
 
 const produceMessages = async () => {
-  //while (true) {
+  while (true) {
     //await process_tbm_data()
-    
-  //}
+    for (let i = 0; i < 3600; i++) {
+      //insertion du delai d'arret dans le json (array.push)
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    //moyenne et producer puis send_messages
+  }
   let arr_messages1 = []
   let arr_messages2 = []
   for (let i = 0; i < 10; i++) {
